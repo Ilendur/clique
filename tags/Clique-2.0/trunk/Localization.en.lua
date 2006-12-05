87630e74c4b078ef38be8d138d708d5142913314
James Whitehead II <jnwhiteh@gmail.com>
2006-12-05 23:11:02 +0000

    Clique: Tagging 2.0 Release

---
+++ tags/Clique-2.0/trunk/Localization.en.lua
@@ -0,0 +1,47 @@
+--[[---------------------------------------------------------------------------------
+    Localisation for English
+----------------------------------------------------------------------------------]]
+
+local L = Clique.Locals
+
+-- This is the default locale.
+	L.RANK                    = "Rank"
+	L.RANK_PATTERN            = "Rank (%d+)"
+	L.CAST_FORMAT             = "%s(Rank %s)"
+
+	L.RACIAL_PASSIVE          = "Racial Passive"
+	L.PASSIVE                 = SPELL_PASSIVE
+	
+	L.CLICKSET_DEFAULT        = "Default"
+	L.CLICKSET_HARMFUL        = "Harmful actions"
+	L.CLICKSET_HELPFUL        = "Helpful actions"
+	L.CLICKSET_OOC            = "Out of Combat"
+	L.CLICKSET_BEARFORM       = "Bear Form"
+	L.CLICKSET_CATFORM        = "Cat Form"
+    L.CLICKSET_AQUATICFORM    = "Aquatic Form"
+	L.CLICKSET_TRAVELFORM     = "Travel Form"
+	L.CLICKSET_MOONKINFORM    = "Moonkin Form"
+	L.CLICKSET_TREEOFLIFE     = "Tree of Life Form"
+	L.CLICKSET_SHADOWFORM     = "Shadowform"
+	L.CLICKSET_STEALTHED      = "Stealthed"
+	L.CLICKSET_BATTLESTANCE   = "Battle Stance"
+	L.CLICKSET_DEFENSIVESTANCE = "Defensive Stance"
+	L.CLICKSET_BERSERKERSTANCE = "Berserker Stance"
+
+	L.BEAR_FORM = "Bear Form"
+	L.DIRE_BEAR_FORM = "Dire Bear Form"
+	L.CAT_FORM = "Cat Form"
+	L.AQUATIC_FORM = "Aquatic Form"
+	L.TRAVEL_FORM = "Travel Form"
+	L.TREEOFLIFE = "Tree of Life"
+	L.MOONKIN_FORM = "Moonkin Form"
+	L.STEALTH = "Stealth"
+	L.SHADOWFORM = "Shadowform"
+	L.BATTLESTANCE = "Battle Stance"
+	L.DEFENSIVESTANCE = "Defensive Stance"
+	L.BERSERKERSTANCE = "Berserker Stance"
+
+	L.BINDING_NOT_DEFINED     = "Binding not defined"
+
+	L.HELP_TEXT               = "Welcome to Clique.  For basic operation, you can navigate the spellbook and decide what spell you'd like to bind to a specific click.  Then click on that spell with whatever click-binding you would like.  For example, navigate to \"Flash Heal\" and shift-LeftClick on it to bind that spell to Shift-LeftClick."
+	L.CUSTOM_HELP             = "This is the Clique custom edit screen.  From here you can configure any of the combinations that the UI makes available to us in response to clicks.  Select a base action from the left column.  You can then click on the button below to set the binding you'd like, and then supply the arguments required (if any)."
